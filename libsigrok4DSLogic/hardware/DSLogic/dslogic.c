754dba0206a45910e41d0082b0b90f021f322c4c
root <dpn@switchvoice.com>
2015-03-14 10:52:35 +0200

    Important FPGA_RLE_LEN comment

--- libsigrok4DSLogic/hardware/DSLogic/dslogic.c
+++ libsigrok4DSLogic/hardware/DSLogic/dslogic.c
@@ -1496,7 +1496,11 @@ static void resubmit_transfer(struct libusb_transfer *transfer)
 
     sr_err("%s: %s", __func__, libusb_error_name(ret));
 }
-
+#define FPGA_RLE_LEN   0xffff  //This is how long currently our RLE buffer in the FPGA
+			       //The capture waits untill we get that many RLE samples.
+			       //Note that this may be a lot of time to get that many RLE 
+			       //for some measured signals. This may be increase to 16M 
+			       //which means hours of recording  
 //The current approach is more a proofe of concept
 void rle_decode(uint16_t *data_rle, unsigned long len_rle, uint16_t *data, unsigned long len){
 	unsigned long first_sample_idx, i,j, k;
@@ -1506,7 +1510,7 @@ void rle_decode(uint16_t *data_rle, unsigned long len_rle, uint16_t *data, unsig
 	while(data_rle[first_sample_idx++] & 0x8000);
 	
 	j=0;
-	for(i=first_sample_idx-1; (j<len) && (i<len_rle); i++){
+	for(i=first_sample_idx-1; (j<len) && (i<min(len_rle, FPGA_RLE_LEN)); i++){
                 if(data_rle[i] & 0x8000){//Count
 			count=data_rle[i]&0x7fff;
                         for(k=0; k<count ;k++){
